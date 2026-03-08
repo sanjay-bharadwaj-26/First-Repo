42707983e331ef013d78cfab83e8b9c592a060a7
Sanjay Bharadwaj <sanjaynarnepati@gmail.com>
2026-03-08 13:50:24 +0530

    Commited Changes

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 <br>
 This is my first GIT Repository
 <br>
-Owner - Sanjay Bharadwaj
+Owner - Sanjay Narnepati
---
+++ untracked.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("Hi ! This is my first GIT HUB Repository");
+
+    return 0;
+}
